fdac2c09c02e7347710c507e1975aae3c6d0f03a
gecko10000 <levtey@gmail.com>
2022-03-29 21:06:15 +0000

    Working without frontend

--- Controllers/CropController.cs
+++ Controllers/CropController.cs
@@ -8,16 +8,100 @@ namespace ImageCropper.Controllers;
 public class FileController : ControllerBase
 {
 
-  private static int index = 0;
+  static FileController()
+  {
+    DeleteAll();
+  }
+
+  private static readonly string DATA_DIR = "data";
+
+  private void MakeStorage()
+  {
+    new DirectoryInfo(DATA_DIR).Create();
+  }
+
+  private bool MakeStorage(Guid guid)
+  {
+    DirectoryInfo dir = new DirectoryInfo(DATA_DIR + "/" + guid);
+    bool alreadyExists = dir.Exists;
+    dir.Create();
+    return !alreadyExists; // true if created, false if already existed
+  }
+
+  private bool Write(Guid guid, MagickImage i, string fileName)
+  {
+    if (Retrieve(guid) is not null)
+    {
+      return false;
+    }
+    i.Write(DATA_DIR + "/" + guid + "/" + fileName);
+    return true;
+  }
+
+  private FileInfo? Retrieve(Guid guid)
+  {
+    DirectoryInfo dir = new DirectoryInfo(DATA_DIR + "/" + guid);
+    if (!dir.Exists)
+    {
+      return null;
+    }
+    FileInfo[] files = dir.GetFiles();
+    return files.Length == 0 ? null : files[0];
+  }
+
+  private void Delete(Guid guid)
+  {
+    DirectoryInfo dir = new DirectoryInfo(DATA_DIR + "/" + guid);
+    dir.Delete(true);
+  }
+
+  public static void DeleteAll()
+  {
+    foreach (DirectoryInfo dir in new DirectoryInfo(DATA_DIR).EnumerateDirectories())
+    {
+      dir.Delete(true);
+    }
+  }
 
   [HttpPost]
-  public IActionResult Upload([FromForm] IFormFile files)
+  public IActionResult Upload([FromForm] List<IFormFile> files)
   {
-    MagickImage i = new MagickImage(files.OpenReadStream());
-    i.Crop(i.Width, i.Height - 20);
-    Stream stream = new MemoryStream();
-    i.Write(stream);
-    stream.Seek(0, SeekOrigin.Begin);
-    return File(stream, "application/octet-stream", "test.png");
+    MakeStorage();
+    List<Guid> ids = new List<Guid>();
+    foreach (IFormFile file in files)
+    {
+      MagickImage i;
+      try
+      {
+        using Stream s = file.OpenReadStream();
+        i = new MagickImage(s);
+      } catch (MagickException){ continue; }
+      Guid guid;
+      do {
+        guid = Guid.NewGuid();
+      } while (Retrieve(guid) is not null);
+      MakeStorage(guid);
+      i.Crop(i.Width, i.Height - 20);
+      if (Write(guid, i, file.FileName))
+      {
+        ids.Add(guid);
+        Task.Delay(1000 * 60 * 10).ContinueWith(t => {
+          Delete(guid);
+        });
+      }
+    }
+    return ids.Count == 0 ? BadRequest("No valid images provided.") : Ok(ids);
+  }
+
+  [HttpGet("{guid}")]
+  public IActionResult Get(string guid)
+  {
+    FileInfo? file = Retrieve(Guid.Parse(guid));
+    if (file is null)
+    {
+      return BadRequest("File not found.");
+    }
+    Stream s = file.OpenRead(); // disposed by return value
+    return File(s, "application/octet-stream");
   }
 }
